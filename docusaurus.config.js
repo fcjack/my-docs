af97abc22fbb9f9836e7e73370481f44dcbe954b
Jackson Coelho <fcjack@gmail.com>
2020-12-14 19:44:50 +0100

    Update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://fcjack.github.io/my-docs/',
-  baseUrl: '/',
+  baseUrl: '/my-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
